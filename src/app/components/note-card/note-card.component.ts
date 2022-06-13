6279db4a735c1ebceba44893074e8c8d09f42909
DESKTOP-JKOEDB1\maco <maciej-ostaszewski@o2.pl>
2022-06-13 09:23:22 +0200

    #trashbin bug fix

--- src/app/components/note-card/note-card.component.ts
+++ src/app/components/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { Note } from 'src/app/models/note';
 import { NotesService } from 'src/app/services/notes-service';
 
@@ -9,16 +9,16 @@ declare const feather: any;
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.css'],
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements AfterViewInit {
   @Input() note?: Note;
 
   constructor(private notesService: NotesService) {}
 
-  ngOnInit() {
-    feather.replace();
-  }
-
   deleteNote() {
     if (this.note) this.notesService.deleteNote(this.note.id);
+  }
+
+  ngAfterViewInit(): void {
+    feather.replace();
   }
 }
